5f1fbb524426cab33c93c369e58c62d5610e5389
Júlio Werner <juliowernermm@gmail.com>
2022-08-18 18:22:12 -0300

    playwright aws lambda

--- src/lib/get-screenshot.ts
+++ src/lib/get-screenshot.ts
@@ -1,18 +1,44 @@
-const playwright = require("playwright-aws-lambda");
+import chromium from "chrome-aws-lambda";
+import playwright from "playwright-core";
+
+export async function getOptions() {
+  const isDev = !process.env.AWS_REGION;
+  let options;
+
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath:
+        "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+      headless: true,
+    };
+  } else {
+    options = {
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    };
+  }
+
+  return options;
+}
 
 export async function getScreenshot(
   html: string,
   { width, height }: { width: number; height: number }
 ) {
-  const browser = await playwright.launchChromium();
-  const context = await browser.newContext();
+  const options = await getOptions();
 
-  const page = await context.newPage();
+  const browser = await playwright.chromium.launch(options);
+
+  const page = await browser.newPage();
 
   await page.setViewportSize({ width, height });
   await page.setContent(html);
 
   const file = await page.screenshot({ type: "png" });
+
+  await browser.close();
 
   return file;
 }
