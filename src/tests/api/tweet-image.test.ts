1c1fd2854138462478da8f22c9c19b78801df499
Júlio Werner <juliowernermm@gmail.com>
2022-08-18 20:43:05 -0300

    request ok

--- src/tests/api/tweet-image.test.ts
+++ src/tests/api/tweet-image.test.ts
@@ -1,9 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test("should return tweet image", async ({ page }, testInfo) => {
-  await page.goto("/api/tweet/1234");
+test("should return tweet image", async ({ page, request }, testInfo) => {
+  const tweetImg = await request.get("/api/tweet/1234");
+  expect(tweetImg.ok()).toBeTruthy();
 
-  expect(page.locator("img")).toBeTruthy();
+  await page.goto("/api/tweet/1234");
 
   await page.screenshot({
     path: `.tests/${testInfo.project.name}/api-tweet-image.png`,
