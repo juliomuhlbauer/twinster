ddead951dca0e1aa1e659d5c986b1bc6f2ec8592
Júlio Werner <juliowernermm@gmail.com>
2022-08-22 22:21:23 -0300

    remove axiom

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const { withAxiom } = require("next-axiom");
 const withPWA = require("next-pwa");
 
-module.exports = withAxiom(
-  withPWA({
-    reactStrictMode: true,
-    swcMinify: true,
+module.exports = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
 
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-    },
-  })
-);
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
+});
