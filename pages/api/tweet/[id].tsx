1830d34f305f87d8bc6baeccbdd240dd8f031524
Júlio Werner <juliowernermm@gmail.com>
2022-08-26 17:02:29 -0300

    symbols

--- pages/api/tweet/[id].tsx
+++ pages/api/tweet/[id].tsx
@@ -13,8 +13,8 @@ import * as ReactDOMServer from "react-dom/server";
 let tweet = welcomeTweet;
 
 const fonts = {
-  heading: `Inter, 'Noto Colr Emoji Glyf', ${base.fonts.heading}`,
-  body: `Inter, 'Noto Colr Emoji Glyf', ${base.fonts.body}`,
+  heading: `Inter, 'Noto Color Emoji Compat', 'Noto Sans Symbols', ${base.fonts.heading}`,
+  body: `Inter, 'Noto Color Emoji Compat', 'Noto Sans Symbols', ${base.fonts.body}`,
 };
 
 const apiTheme = extendTheme(theme, {
@@ -43,7 +43,11 @@ const tweetImage = async (req: NextApiRequest, res: NextApiResponse) => {
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Colr+Emoji+Glyf&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji+Compat&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Symbols:wght@500&display=swap"
           rel="stylesheet"
         />
       </head>
