1fb9b3d3e8e20976c8ff7caff63a305442ae5fee
Júlio Werner <juliowernermm@gmail.com>
2022-08-26 15:20:22 -0300

    emoji font

--- pages/api/tweet/[id].tsx
+++ pages/api/tweet/[id].tsx
@@ -33,6 +33,10 @@ const tweetImage = async (req: NextApiRequest, res: NextApiResponse) => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Emoji:wght@500&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <ChakraProvider theme={theme}>
         <Tweet
